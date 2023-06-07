4c7aa1ae82879e6189f73ddbe2128ae9f1b6b598
Kled Chen <kled.chen@zenlayer.com>
2023-06-07 22:46:39 +0800

    trigger workflow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(456);
+        System.out.println("Hello workflows!");
 
         System.out.println("aaa");
     }
